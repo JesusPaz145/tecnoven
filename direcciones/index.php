a4d84409fb88189ede82349c21163526da472fb2
Jesus Paz <44905310+JesusPaz145@users.noreply.github.com>
2022-12-28 17:02:21 -0400

    pull

--- direcciones/index.php
+++ direcciones/index.php
@@ -5,9 +5,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Direcciones</title>
+    <script src="js/push.min.js"></script>
     
-</head>
+</head> 
 <body>
+    <script>
+        Push.create("Hola Mundo",{
+            body:"Este es el cuerpo de la notificacion",
+            icon: "images/logo.png",
+            timeout: 5000,
+            onClick: function () {
+                window.focus();
+                this.close();
+            }
+        });
+    </script>
     <h1>Direcciones</h1>
     <?php
     include('mostrar.php');
